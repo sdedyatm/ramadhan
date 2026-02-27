fe5cf5f83c62c963882f122cd4cd5c4bb9f9f1de
sdedyatm <11027164+sdedyatm@users.noreply.github.com>
2026-02-27 10:09:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 
 "use strict";
 
-const CACHE_VERSION = "v1.3.4";
+const CACHE_VERSION = "v1.3.5";
 const STATIC_CACHE = `propwa-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `propwa-dynamic-${CACHE_VERSION}`;
 const OFFLINE_URL = "./offline-fallback.html";
